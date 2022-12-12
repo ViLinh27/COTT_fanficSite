3d256d305ae55dc0d35914d60c22be2c8f48bbe6
Vi-Linh Nguyen <vilinh27@gmail.com>
2022-12-11 21:13:24 -0600

    install material ui and fix styling base

--- fanfic-site/src/App.js
+++ fanfic-site/src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        header
-      </header>
+      <h1>Class of the Titans - The Ghost Queen</h1>
+      <h2>Original show by Studio B</h2>
+      <h3>Fanfiction by Linh Nguyen</h3>
     </div>
   );
 }
