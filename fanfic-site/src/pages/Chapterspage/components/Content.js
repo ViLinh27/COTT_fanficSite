ac35caf1fcd2638bf483bb46763552095fcfd11e
Vi-Linh Nguyen <vilinh27@gmail.com>
2025-04-10 10:58:20 -0500

    add previews for chapters and fix some box sizing

--- fanfic-site/src/pages/Chapterspage/components/Content.js
+++ fanfic-site/src/pages/Chapterspage/components/Content.js
@@ -10,17 +10,22 @@ function Content(){
             <Intro/>
 
             <div className="chapterlist_container">
+                
                 <div className="chapter-header">
                     <h5>Chapter One: Cronus has new underlings</h5>
                 </div>
                 <div className="chapter-info">
                   <table>
-                    {/* summary */}
-                    <tr><th>Summary</th></tr>
-                    <td><p>....</p></td>
                     {/* preview */}
                     <tr><th>Preview</th></tr>
-                    <td><p>...</p></td>
+                      <td>
+                        <p>
+                          Seven teenagers adept in various types of magic were now working for Cronus. Their victory against Jay and his friends looked like a guarantee at that point.
+                        </p>
+                        <p>
+                          Theresa could not predict their movements even with her psychic abilities. She thought they even had their own psychic. Herry's strength couldn't outmatch the brute force of the earth magic used against them by the girl in the purple hat. What did they call her? Oh, right- Pomelia. Archie and Atlanta had both been knocked out. Even Neil's luck could not help them now.
+                        </p>
+                      </td>
                     {/* link to wattpad */}
                     <tr>
                       <a href="https://www.wattpad.com/798036683-the-ghost-queen-chapter-one-cronus-has-new">
@@ -35,12 +40,16 @@ function Content(){
                 </div>
                 <div className="chapter-info">
                   <table>
-                    {/* summary */}
-                    <tr><th>Summary</th></tr>
-                    <td><p>....</p></td>
                     {/* preview */}
                     <tr><th>Preview</th></tr>
-                    <td><p>...</p></td>
+                    <td>
+                      <p>
+                        The Athenian pantheon of ancient times had been one of humanities most ambitious offerings to the gods when the world was young. But now it was no more than a clichéd tourist attraction for unoriginal adventure seekers.
+                      </p>
+                      <p>
+                        At night though, the ruins were closed to outsiders. Tonight of all nights proved tedious to navigate through the ancient pantheon. The clouds covered the moon from an earlier rain storm. The ancient temple's stone hardly reflected enough light for any late night tourist to sneak around, despite the moisture giving the surroundings a slight shine. Fortunately, a ghostly fate did not need of such frivolities. One fate was here now, waiting by a column.
+                      </p>
+                    </td>
                     {/* link to wattpad */}
                     <tr>
                       <a href="">
@@ -55,12 +64,16 @@ function Content(){
                 </div>
                 <div className="chapter-info">
                   <table>
-                    {/* summary */}
-                    <tr><th>Summary</th></tr>
-                    <td><p>....</p></td>
                     {/* preview */}
                     <tr><th>Preview</th></tr>
-                    <td><p>...</p></td>
+                    <td>
+                      <p>
+                        The heroes returned to Olympus High's secret pantheon exhausted, defeated and barely conscious.  Herry had Atlanta and Archie over each shoulder. Theresa leaned on Jay for support. Neil and Odie leaned on each other. 
+                      </p>
+                      <p>
+                        Hermes instantly grew concerned over the state of the young heroes. He called Hera and Chiron and flew out of his office as fast as the messenger god could. The portal behind the heroes closed back into its caduceus.
+                      </p>
+                    </td>
                     {/* link to wattpad */}
                     <tr>
                       <a href="">
@@ -75,12 +88,16 @@ function Content(){
                 </div>
                 <div className="chapter-info">
                   <table>
-                    {/* summary */}
-                    <tr><th>Summary</th></tr>
-                    <td><p>....</p></td>
                     {/* preview */}
                     <tr><th>Preview</th></tr>
-                    <td><p>...</p></td>
+                    <td>
+                      <p>
+                        The new students at Olympus High were set to start their first day today. Jay told his friends to be ready. Knowing Cronus, he pulled some strings for his new acolytes to be able to attend their school.
+                      </p>
+                      <p>
+                        Herry stood by his locker retrieving some extra books Hera advised would help in some remedial studies. He looked at the progress report on top of the stack. Granny would not be happy about the results.
+                      </p>
+                      </td>
                     {/* link to wattpad */}
                     <tr>
                       <a href="">
@@ -95,12 +112,16 @@ function Content(){
                 </div>
                 <div className="chapter-info">
                   <table>
-                    {/* summary */}
-                    <tr><th>Summary</th></tr>
-                    <td><p>....</p></td>
                     {/* preview */}
                     <tr><th>Preview</th></tr>
-                    <td><p>...</p></td>
+                    <td>
+                      <p>
+                        Herry had watched the short haired girl in the purple hat jog across the local park trail in her lilac colored track suit. He had decided to follow her.
+                      </p>
+                      <p>
+                        Now Herry found himself approaching her while she sat catching her breath under a big oak tree. She leaned against the tree, face towards the sky and eyes closed. Her breathing returned to a steady pace. Now that he got closer Herry realized she wasn't actually wearing her hat today. That was a headband. How did he mistake that? Weird. Trick of the light he guessed.
+                      </p>
+                    </td>
                     {/* link to wattpad */}
                     <tr>
                       <a href="">
@@ -115,12 +136,12 @@ function Content(){
                 </div>
                 <div className="chapter-info">
                   <table>
-                    {/* summary */}
-                    <tr><th>Summary</th></tr>
-                    <td><p>....</p></td>
                     {/* preview */}
                     <tr><th>Preview</th></tr>
-                    <td><p>...</p></td>
+                    <td>
+                      <p>Faunus, a new vegetarian restaurant not far from the brownstone, just opened up. The couple who owned the place advertised their establishment as fusion Mediterranean. Although one of them was responsible for putting mostly Greek on the menu. The wait staff was comprised of students from Romulus high school. Seven of them happened to live with the owners.</p>
+                      <p>The bell by the entrance rung. A group of teenagers walked into the restaurant. Atlanta and Theresa had wanted to check out the new trendy hang out spot they heard about. Neil and Herry were the only boys who were willing to join them. The girls' boyfriends found other activities to occupy them, to the frustration of both Theresa and Atlanta. Odie had an invention he wanted to run by Hermes and Hephaestus. </p>
+                    </td>
                     {/* link to wattpad */}
                     <tr>
                       <a href="">
@@ -135,12 +156,12 @@ function Content(){
                 </div>
                 <div className="chapter-info">
                   <table>
-                    {/* summary */}
-                    <tr><th>Summary</th></tr>
-                    <td><p>....</p></td>
                     {/* preview */}
                     <tr><th>Preview</th></tr>
-                    <td><p>...</p></td>
+                    <td>
+                      <p>When the young heroes returned to the brownstone, Theresa had called Atlanta to her room to talk privately. She explained the vision she saw earlier at Faunus to Atlanta. Atlanta wondered if they should talk to Jay about it.</p>
+                      <p>"No. Not now." Theresa bit her lip and looked down. Instinct told her they should wait a bit to see if Herry would say anything, but logic said Jay should know about this.</p>
+                    </td>
                     {/* link to wattpad */}
                     <tr>
                       <a href="">
@@ -155,12 +176,16 @@ function Content(){
                 </div>
                 <div className="chapter-info">
                   <table>
-                    {/* summary */}
-                    <tr><th>Summary</th></tr>
-                    <td><p>....</p></td>
                     {/* preview */}
                     <tr><th>Preview</th></tr>
-                    <td><p>...</p></td>
+                    <td>
+                      <p>
+                        Hannah and Hailey had shut themselves in the garage working on some tech to help track the Olympians' group of young heroes. The blowtorch and soddering iron could be heard from inside their brownstone. Komos took over the kitchen to prepare enough dinner for seven teenagers, a satyr, a dryad and an always hungry underworld hound.
+                      </p>
+                      <p>
+                        "What's your progress children?" Komos yelled, stirring a bechamel sauce for some bulk moussaka.
+                      </p>
+                      </td>
                     {/* link to wattpad */}
                     <tr>
                       <a href="">
@@ -171,17 +196,21 @@ function Content(){
                 </div>
 
                 <div className="chapter-header">
-                    <h5>Chapter Nine: Trouble in Pradise
+                    <h5>Chapter Nine: Trouble in Paradise
                     for the chaos couple</h5>
                 </div>
                 <div className="chapter-info">
                   <table>
-                    {/* summary */}
-                    <tr><th>Summary</th></tr>
-                    <td><p>....</p></td>
                     {/* preview */}
                     <tr><th>Preview</th></tr>
-                    <td><p>...</p></td>
+                    <td>
+                      <p>
+                        Archie and Atlanta had been fighting more ever since their encounter with Cronus's new band of magicians and alchemists. Before the arguments just looked like their usual rival banter that bordered on flirting (as they were officially dating now, since the whole incident with the phantom).
+                      </p>
+                      <p>
+                        Now though. Now the banter went from occasional, still-friendly argument to full on shouting matches. Atlanta and Archie hadn't been this aggressive to each other since a corrupted Eros infected them with hate.
+                      </p>
+                    </td>
                     {/* link to wattpad */}
                     <tr>
                       <a href="">
@@ -196,12 +225,16 @@ function Content(){
                 </div>
                 <div className="chapter-info">
                   <table>
-                    {/* summary */}
-                    <tr><th>Summary</th></tr>
-                    <td><p>....</p></td>
                     {/* preview */}
                     <tr><th>Preview</th></tr>
-                    <td><p>...</p></td>
+                    <td>
+                      <p>
+                        Archie found himself smiling again when talking to Patrick in this new restaurant he only would have known about thanks to Atlanta. Patrick had recommended Archie the pineapple mango smoothie and the refreshing tang did improve Archie's mood.
+                      </p>
+                      <p>
+                        "So how did you end up in New Olympia?" Archie asked Patrick.
+                      </p>
+                    </td>
                     {/* link to wattpad */}
                     <tr>
                       <a href="">
@@ -216,12 +249,16 @@ function Content(){
                 </div>
                 <div className="chapter-info">
                   <table>
-                    {/* summary */}
-                    <tr><th>Summary</th></tr>
-                    <td><p>....</p></td>
                     {/* preview */}
                     <tr><th>Preview</th></tr>
-                    <td><p>...</p></td>
+                    <td>
+                      <p>
+                        Erato finished roasting her eighth pot roast . She called her husband asking for the timeframe she had left before the transformations started.
+                      </p>
+                      <p>
+                        Eight hours. Fitting. Eight hours left and the eight pot roasts were done. Erato smiled at the timeliness. She went to check on her husband, carrying her tray of eight pot roasts. Just as she did every month since these strange teenagers arrived to the Ariti dormitories, the building they owned. Komos collared the last of the four werewolf children to their respective "full moon rooms" deep in the building's basement.
+                      </p>
+                    </td>
                     {/* link to wattpad */}
                     <tr>
                       <a href="">
@@ -237,12 +274,16 @@ function Content(){
                 </div>
                 <div className="chapter-info">
                   <table>
-                    {/* summary */}
-                    <tr><th>Summary</th></tr>
-                    <td><p>....</p></td>
                     {/* preview */}
                     <tr><th>Preview</th></tr>
-                    <td><p>...</p></td>
+                    <td>
+                      <p>
+                        Pomelia Fuentes was a vampire. Odie and Neil were sure of it. Jay and Theresa seemed more uncertain. Several days of research into Cronus's new underlings and this is what they managed to find? Jay had a hard time hiding his disappointment. The video game break seemed to have helped Odie's motivation but Jay hoped for more conclusive information to work with.
+                      </p>
+                      <p>
+                        "How does this help us?" Jay asked.
+                      </p>
+                      </td>
                     {/* link to wattpad */}
                     <tr>
                       <a href="">
@@ -251,7 +292,32 @@ function Content(){
                     </tr>
                   </table>
                 </div>
-            </div>
+
+                <div className="chapter-header">
+                    <h5>Chapter Thirteen: Reconnaisance Mission</h5>
+                </div>
+                <div className="chapter-info">
+                  <table>
+                    {/* preview */}
+                    <tr><th>Preview</th></tr>
+                    <td>
+                      <p>
+                        Herry managed to notice the figure behind him and Pomelia before she did. He burst out from his library chair and ran towards the teenager with the phone. The chair tumbling to the library floor nearly echoed within the empty library. Herry grabbed him by the collar. Pomelia turned towards where Herry ran.
+                      </p>
+                      <p>
+                        "What was that?" Herry's face inches away from whoever this was watching him and his tutor.
+                      </p>
+                    </td>
+                    {/* link to wattpad */}
+                    <tr>
+                      <a href="">
+                        <p>Read More</p>
+                      </a>
+                    </tr>
+                  </table>
+                </div>
+           
+            </div>   
           </div>
         </>
     );
